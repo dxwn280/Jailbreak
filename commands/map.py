beb881c4b9656eb31ad69efbb5864c88fc02fc02
dxwn <jeffery92022@gmail.com>
2024-04-05 06:20:47 +0100

    Create map.py

---
+++ commands/map.py
@@ -0,0 +1,69 @@
+import os
+import discord
+import requests
+import xml.etree.ElementTree as ET
+import json
+import aiohttp
+from discord.ext import commands
+from discord import app_commands
+from discord_webhook import DiscordWebhook
+
+intents = discord.Intents.all()
+intents.message_content = True
+client = commands.Bot(command_prefix="/", intents=intents)
+
+with open('config.json') as config_file:
+    config_data = json.load(config_file)
+WEBHOOKURL = config_data['webhook']
+
+# XML STUFF # Function to scrape XML data from API
+XML_API_URL = "https://edgegamers.gameme.com/api/serverinfo/104.128.58.156:27015/players/status"
+
+def scrape_xml():
+    response = requests.get(XML_API_URL)
+    if response.status_code == 200:
+        root = ET.fromstring(response.content)
+        # Find the <map> element inside <server> inside <serverinfo>
+        map_data = root.find('.//server/map').text
+        player_data = root.find('.//server/act').text
+        return map_data, player_data
+    else:
+        return None, None  # Returning None values if fetching XML data fails
+
+
+
+# Command: /map
+@client.hybrid_command(help="Displays the current map of the game server", usage="/map")
+async def map(ctx: commands.Context):
+    if ctx.guild is None:
+        guild_name = "Private Messages"
+    else:
+        guild_name = ctx.guild.name  # Access the name attribute to get the guild name
+
+    # Get the current timestamp in UTC
+    current_time = discord.utils.utcnow().strftime('%Y-%m-%d %H:%M:%S')
+
+    detective_info = f'{current_time}: {ctx.author} ({ctx.author.id}) used the command: /map in ({guild_name})' #this is for the webhook
+
+    map_info, player_info = scrape_xml()  # Call scrape_xml() once and unpack the tuple
+    if map_info is not None and player_info is not None:
+        # Convert player_info to integer for comparison
+        player_count = int(player_info)
+        if player_count > 0:
+            message = 'Map: ' + map_info + ' | Players: ' + player_info
+            await ctx.send(message)
+        if player_count < 0:
+            await ctx.send('Game is down, no players are currently online.')
+        else:
+            message = 'Players: ' + player_info + '\nServer either has no players or gameme/server is down'
+            await ctx.send(message)
+
+    # Print detective_info to the terminal
+    print(detective_info)
+
+    webhook_url = WEBHOOKURL 
+    webhook = DiscordWebhook(url=webhook_url, content=detective_info) #this sends webhooks
+    response = webhook.execute()
+
+    return detective_info
+
