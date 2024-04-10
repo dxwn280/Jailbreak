af9a81207c1170faa835133ea91f45c420b0f591
dxwn <jeffery92022@gmail.com>
2024-04-10 14:11:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import discord
 import json
 from discord.ext import commands
-from commands import map as map_commands
+from commands import map as map_command
+from commands import players as players_command
 
 intents = discord.Intents.all()
 intents.message_content = True
@@ -10,13 +11,16 @@ client = commands.Bot(command_prefix="/", intents=intents)
 with open('config.json') as config_file:
     config_data = json.load(config_file)
 TOKEN = config_data['token']
+OWNER = int(config_data['owner'])
+
+# Import and register the map commands from map.py
+client.add_command(map_command.map)
+client.add_command(players_command.players)
 
 # Event: Bot is ready
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user.name}')
-
-# Import and register the map commands from map.py
-client.add_command(map_commands.map)
+    await client.tree.sync() #this syncs all the commands available to the command tree on start
 
 client.run(TOKEN)
