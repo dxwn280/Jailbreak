33f58f9ce58d6ff62ec5e9a81c6dbd04a4717ed2
dxwn <jeffery92022@gmail.com>
2024-04-14 15:14:52 +0100

    Update players.py

--- commands/players.py
+++ commands/players.py
@@ -8,15 +8,29 @@ from discord.ext import commands
 from discord import app_commands
 from discord_webhook import DiscordWebhook
 
-intents = discord.Intents.all()
-intents.message_content = True
-client = commands.Bot(command_prefix="/", intents=intents)
-
 with open('config.json') as config_file:
     config_data = json.load(config_file)
 WEBHOOKURL = config_data['webhook']
 
 XML_API_URL = 'https://edgegamers.gameme.com/api/serverinfo/104.128.58.156:27015/players'
+
+intents = discord.Intents.all()
+intents.message_content = True
+client = commands.Bot(command_prefix="/", intents=intents)
+
+image_links = {
+    "avalanche": "https://i.imgur.com/ZfbV9vJ.png",
+    "undertale": "https://i.imgur.com/CzzwLyF.png",
+    "clouds": "https://i.imgur.com/qWntm6o.png",
+    "minecraft": "https://i.imgur.com/3f27kd7.png",
+    "quake": "https://i.imgur.com/lcPnokt.png",
+    "spy": "https://i.imgur.com/PZUOLg8.png",
+    "peanut": "https://i.imgur.com/t0bTOQv.png",
+    "vipinthemix": "https://i.imgur.com/IB7E1d2.png",
+    "kwejsi": "https://i.imgur.com/gza8kYA.png",
+    "umbrella": "https://i.imgur.com/0HLqFuo.png",
+    "uknown": "https://media.discordapp.net/attachments/1152808650436522045/1228862165105250325/video.gif?ex=662d9613&is=661b2113&hm=db8815275b4e99ae71163ce0640e050dd112579bb9adad4452455cc476f9a571&", #whav is cute
+}
 
 def scrape_xml():
     response = requests.get(XML_API_URL)
@@ -24,12 +38,10 @@ def scrape_xml():
         root = ET.fromstring(response.content)
 
         # Find the <map> element
-        map_info = root.find('.//server/map').text
-
-        # Initialize empty lists for both teams
-        ct_player = []
-        t_player = []
-
+        mapinfo = root.find('.//server/map').text
+        # Initialize empty lists for both team
+        ctlist = []
+        tlist = []
         # Iterate through each player
         for player in root.findall('.//server/players/player'):
             player_name = player.find('name').text
@@ -37,19 +49,30 @@ def scrape_xml():
 
             # Depending on the team, append the player to the corresponding list
             if player_team == "CT":
-                ct_player.append(player_name)
+                ctlist.append(player_name)
             elif player_team == "TERRORIST":
-                t_player.append(player_name)
-            # Optionally handle players without a team or with a different designation
-
-        return map_info, ct_player, t_player
+                tlist.append(player_name)
+        return mapinfo, tlist, ctlist
     else:
         return None, None, None  # Returning None values if fetching XML data fails
 
 
-# Command: /players
-@client.hybrid_command(help="Displays players on the server", usage="/players")
-async def players(ctx: commands.Context):
+mapinfo = str(scrape_xml())
+image_data = ['avalanche', 'undertale', 'clouds', 'minecraft', 'quake', 'spy', 'peanut', 'vipinthemix', 'kwejsi', 'umbrella']
+split_info = mapinfo.split('_')
+
+check =  any(item in image_data for item in split_info)
+ 
+if check is True:
+    image = image_links[split_info[1]] #dont like the way this is done, should find a way to just compare a subset to a set and find whats matching, but not sure how
+else:
+    image = image_links["uknown"] #cute whav gif
+
+
+@client.hybrid_command(usage="/players")
+async def players(ctx: discord.Member = None):
+    current_time = discord.utils.utcnow() # this has to be an object, before I had: current_time = discord.utils.utcnow().strftime('%Y-%m-%d %H:%M:%S') . but this didnt work
+                                          # probably because of the ".strftime('%Y-%m-%d %H:%M:%S')" part which i think turned it into a string
     if ctx.guild is None:
         guild_name = "Private Messages"
     else:
@@ -58,26 +81,80 @@ async def players(ctx: commands.Context):
         guild_ownerid = ctx.guild.owner_id
         guild_owner = ctx.guild.owner
 
-    current_time = discord.utils.utcnow().strftime('%Y-%m-%d %H:%M:%S')
-
     if ctx.guild is None:
         detective_info = f'~~~\n{current_time} \n{ctx.author} ({ctx.author.id}) Used the command: /players \nServer: ({guild_name})'
     else:
         detective_info = f'~~~\n{current_time} \n{ctx.author} ({ctx.author.id}) Used the command: /players \nName: ({guild_name}) \nID: ({guild_id}) \nOwner: ({guild_owner}) \nID: ({guild_ownerid})'
 
-    map_info, ct_player, t_player = scrape_xml()
-    if ct_player and t_player:  # Changed from `is not None` to just `ct_player and t_player` to check for non-empty
-        message = f"Map: {map_info}\n\nCT List:\n"
-        for player in ct_player:
-            message += f"{player}\n"
+    # theres a better way to do this, could use a for loop to go through each player and asign them to their list while assigning teams & names,
+    # just not sure how to assign last 2 ppl to the first & second list when (players) % 3 != 0
+    map_info, tlist, ctlist = scrape_xml()
+    if ctlist and tlist: 
+        midpoint1 = len(ctlist)//2
+        midpoint2 = len(tlist)//3
+    
+        if len(ctlist) % 2 == 0:
+            ctlist1 = ctlist[:midpoint1]
+            ctlist2 = ctlist[midpoint1:]
+        elif len(ctlist) % 2 != 0:
+            ctlist1 = ctlist[:midpoint1+1]
+            ctlist2 = ctlist[midpoint1+1:]
 
-        message += "\nT List:\n"
-        for player in t_player:
-            message += f"{player}\n"
+        if len(tlist) % 3 == 0:
+            tlist1 = tlist[:midpoint2]
+            tlist2 = tlist[midpoint2:midpoint2*2]
+            tlist3 = tlist[midpoint2*2:]
+        elif len(tlist) % 3 == 1:
+            tlist1 = tlist[:midpoint2] + tlist[midpoint2*3::]
+            tlist2 = tlist[midpoint2:midpoint2*2]
+            tlist3 = tlist[midpoint2*2:midpoint2*3]
+        elif len(tlist) % 3 == 2:
+            tlist1 = tlist[:midpoint2] + tlist[midpoint2*3::2]
+            tlist2 = tlist[midpoint2:midpoint2*2] + tlist[midpoint2*3+1]
+    
 
-        await ctx.send(message)
+        embed = discord.Embed(
+            title=f"Click to connect\nMap: {map_info}",
+            url="https://cs2browser.com/connect/104.128.58.156:27015",
+            timestamp=current_time
+        )
+
+        embed.add_field(
+            name="CT Players",
+            value='\n'.join(map(str, ctlist1)),
+            inline=True
+            )
+        embed.add_field(
+            name="\u200b",
+            value='\n'.join(map(str, ctlist2)),
+            inline=True
+            )
+        embed.add_field(
+            name="\u200b",
+            value=f"",
+            inline=True
+        )
+
+        embed.add_field(
+            name="T Players",
+            value='\n'.join(map(str, tlist1)),
+            inline=True
+            )
+        embed.add_field(
+            name="\u200b",
+            value='\n'.join(map(str, tlist2)),
+            inline=True
+            )
+        embed.add_field(
+            name="\u200b",
+            value='\n'.join(map(str, tlist3)),
+            inline=True
+        )
+        embed.set_image(url=image)
+        await ctx.send(embed=embed)
+
     else:
-        await ctx.send(f"Map: {map_info}\nPlayers: 0\nServer is dead or gameme/server is down.")
+        await ctx.send(f"Map: {map_info}\nPlayers: 0\nServer is dead or gameme/server is down. \nhttps://i.imgur.com/ZQFv2cq.mp4")
 
     print(detective_info)
     webhook_url = WEBHOOKURL
@@ -85,4 +162,3 @@ async def players(ctx: commands.Context):
     response = webhook.execute()
 
     return detective_info
-
